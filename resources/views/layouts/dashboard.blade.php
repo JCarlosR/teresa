1e7f147198d69f0121d8ad4eeb95042977008f55
Juan Carlos <juancagb.17@gmail.com>
2016-12-02 15:58:22 -0500

    Menu options updated

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -166,27 +166,19 @@
                 <h4 class="media-heading fs-16">Ryan Moreno</h4>
                 <div class="dropdown"><a id="dropdown-status" href="javascript:;" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="true" class="dropdown-toggle"><span class="status bg-success"></span> Online <span class="caret"></span></a>
                     <ul aria-labelledby="dropdown-status" class="dropdown-menu animated fadeInUp">
-                        <li><a href="#">Offline</a></li>
-                        <li><a href="#">Busy</a></li>
-                        <li><a href="#">Away</a></li>
+                        <li><a href="#">Desconectado</a></li>
+                        <li><a href="#">Ocupado</a></li>
                         <li role="separator" class="divider"></li>
-                        <li><a href="login.html">Logout</a></li>
+                        <li><a href="{{ url('/logout') }}">Cerrar sesión</a></li>
                     </ul>
                 </div>
             </div>
         </div>
-            {{--<a class="list-group-item" href="{{ url('/datos/contacto') }}">Datos de contacto</a>--}}
-            {{--<a class="list-group-item" href="{{ url('/datos/acceso') }}">Accesos al servidor</a>--}}
-            {{--<a class="list-group-item" href="{{ url('/datos/sociales') }}">Perfiles sociales</a>--}}
-            {{--<a class="list-group-item" href="{{ url('/datos/perfiles') }}">Perfiles profesionales</a>--}}
-
             {{--<a class="list-group-item" href="{{ url('/datos/personal') }}">Personal</a>--}}
 
             {{--<a class="list-group-item" href="{{ url('/proyectos') }}">Proyectos</a>--}}
-            {{--<a class="list-group-item" href="{{ url('/pagos') }}">Pagos</a>--}}
-            {{--<a class="list-group-item" href="{{ url('/resumen') }}">Resumen</a>--}}
         <ul class="list-unstyled navigation mb-0">
-            <li class="sidebar-category">Main</li>
+            <li class="sidebar-category">Opciones principales</li>
             <li class="panel">
                 <a href="{{ url('/home') }}">
                     <i class="ion-ios-home-outline bg-purple"></i>
@@ -194,58 +186,32 @@
                 </a>
             </li>
             <li class="panel">
-                <a href="{{ url('/datos/principales') }}">
+                <a role="button" data-toggle="collapse" data-parent=".navigation" href="#collapse1" aria-expanded="false" aria-controls="collapse1" class="collapsed">
                     <i class="ion-ios-person-outline bg-success"></i>
-                    <span class="sidebar-title">Datos principales</span>
+                    <span class="sidebar-title">Datos</span>
+                </a>
+                <ul id="collapse1" class="list-unstyled collapse">
+                    <li><a href="{{ url('/datos/principales') }}">Datos principales</a></li>
+                    <li><a href="{{ url('/datos/contacto') }}">Datos de contacto</a></li>
+                    <li><a href="{{ url('/datos/acceso') }}">Acceso a servidores</a></li>
+                    <li><a href="{{ url('/perfiles/sociales') }}">Perfiles sociales</a></li>
+                    <li><a href="{{ url('/perfiles/profesionales') }}">Perfiles profesionales</a></li>
+                </ul>
+            </li>
+            <li class="panel">
+                <a href="{{ url('/pagos') }}">
+                    <i class="ion-ios-calendar-outline bg-info"></i>
+                    <span class="sidebar-title">Pagos</span>
                 </a>
             </li>
-            <li class="panel"><a href="chat-dashboard.html"><i class="ion-ios-chatbubble-outline bg-primary"></i><span class="sidebar-title">Chat dashboard</span></a></li>
-            <li class="panel"><a href="calendar.html"><i class="ion-ios-calendar-outline bg-info"></i><span class="sidebar-title">Calendar</span></a></li>
-            <li class="panel"><a role="button" data-toggle="collapse" data-parent=".navigation" href="#collapse2" aria-expanded="false" aria-controls="collapse2" class="collapsed"><i class="ion-ios-printer-outline bg-danger"></i><span class="sidebar-title">Email</span></a>
-                <ul id="collapse2" class="list-unstyled collapse">
-                    <li><a href="email-inbox.html">Inbox</a></li>
-                    <li><a href="email-read.html">Read email</a></li>
-                    <li><a href="email-compose.html">Compose email</a></li>
-                </ul>
+            <li class="panel">
+                <a href="{{ url('/personal') }}">
+                    <i class="ion-ios-people bg-warning"></i>
+                    <span class="sidebar-title">Personal</span>
+                </a>
             </li>
-            <li class="panel"><a href="widgets.html"><i class="ion-ios-albums-outline bg-warning"></i><span class="sidebar-title">Widgets</span></a></li>
-            <li class="sidebar-category">Menu Levels</li>
-            <li class="panel"><a role="button" data-toggle="collapse" data-parent=".navigation" href="#collapse20" aria-expanded="false" aria-controls="collapse20" class="collapsed"><i class="ion-ios-folder-outline bg-black"></i><span class="sidebar-title">Menu levels</span></a>
-                <ul id="collapse20" class="list-unstyled collapse">
-                    <li><a role="button" data-toggle="collapse" data-parent="#collapse20" href="#collapse201" aria-expanded="false" aria-controls="collapse201" class="collapsed">Menu level 2.1</a>
-                        <ul id="collapse201" class="list-unstyled collapse">
-                            <li><a href="#">Menu level 3.1</a></li>
-                            <li><a href="#">Menu level 3.2</a></li>
-                            <li><a href="#">Menu level 3.3</a></li>
-                        </ul>
-                    </li>
-                    <li><a role="button" data-toggle="collapse" data-parent="#collapse20" href="#collapse202" aria-expanded="false" aria-controls="collapse202" class="collapsed">Menu level 2.2</a>
-                        <ul id="collapse202" class="list-unstyled collapse">
-                            <li><a href="#">Menu level 3.4</a></li>
-                            <li><a role="button" data-toggle="collapse" data-parent="#collapse202" href="#collapse302" aria-expanded="false" aria-controls="collapse302" class="collapsed">Menu level 3.5</a>
-                                <ul id="collapse302" class="list-unstyled collapse">
-                                    <li><a href="#">Menu level 4.1</a></li>
-                                    <li><a role="button" data-toggle="collapse" data-parent="#collapse302" href="#collapse402" aria-expanded="false" aria-controls="collapse402" class="collapsed">Menu level 4.2</a>
-                                        <ul id="collapse402" class="list-unstyled collapse">
-                                            <li><a href="#">Menu level 5.1</a></li>
-                                            <li><a href="#">Menu level 5.2</a></li>
-                                        </ul>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li><a href="#">Menu level 3.6</a></li>
-                        </ul>
-                    </li>
-                    <li><a role="button" data-toggle="collapse" data-parent="#collapse20" href="#collapse203" aria-expanded="false" aria-controls="collapse203" class="collapsed">Menu level 2.3</a>
-                        <ul id="collapse203" class="list-unstyled collapse">
-                            <li><a href="#">Menu level 3.7</a></li>
-                            <li><a href="#">Menu level 3.8</a></li>
-                            <li><a href="#">Menu level 3.9</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
-            <li class="sidebar-category">Components</li>
+
+            <li class="sidebar-category">Más opciones</li>
             <li class="panel"><a role="button" data-toggle="collapse" data-parent=".navigation" href="#collapse6" aria-expanded="false" aria-controls="collapse6" class="collapsed"><i class="ion-ios-list-outline bg-black"></i><span class="sidebar-title">UI Elements</span></a>
                 <ul id="collapse6" class="list-unstyled collapse">
                     <li><a href="animations.html">Animations</a></li>
@@ -271,22 +237,6 @@
                     <li><a href="text-editors.html">Text editors</a></li>
                 </ul>
             </li>
-            <li class="panel"><a role="button" data-toggle="collapse" data-parent=".navigation" href="#collapse10" aria-expanded="false" aria-controls="collapse10" class="collapsed"><i class="ion-ios-ionic-outline bg-black"></i><span class="sidebar-title">Icons</span></a>
-                <ul id="collapse10" class="list-unstyled collapse">
-                    <li><a href="glyphicons.html">Glyphicons</a></li>
-                    <li><a href="font-awesome.html">Font awesome</a></li>
-                    <li><a href="material-design.html">Material design</a></li>
-                    <li><a href="themify.html">Themify</a></li>
-                    <li><a href="weather-icons.html">Weather</a></li>
-                    <li><a href="flag-icons.html">Flag</a></li>
-                </ul>
-            </li>
-            <li class="panel"><a role="button" data-toggle="collapse" data-parent=".navigation" href="#collapse11" aria-expanded="false" aria-controls="collapse11" class="collapsed"><i class="ion-ios-browsers-outline bg-black"></i><span class="sidebar-title">Tables</span></a>
-                <ul id="collapse11" class="list-unstyled collapse">
-                    <li><a href="basic-tables.html">Basic tables</a></li>
-                    <li><a href="data-tables.html">Data tables</a></li>
-                </ul>
-            </li>
             <li class="panel"><a role="button" data-toggle="collapse" data-parent=".navigation" href="#collapse12" aria-expanded="false" aria-controls="collapse12" class="collapsed"><i class="ion-ios-pie-outline bg-black"></i><span class="sidebar-title">Charts</span></a>
                 <ul id="collapse12" class="list-unstyled collapse">
                     <li><a href="flot-charts.html">Flot charts</a></li>
@@ -302,29 +252,16 @@
                     <li><a href="google-maps.html">Google maps</a></li>
                 </ul>
             </li>
-            <li class="sidebar-category">Page kits</li>
-            <li class="panel"><a role="button" data-toggle="collapse" data-parent=".navigation" href="#collapse14" aria-expanded="false" aria-controls="collapse14" class="collapsed"><i class="ion-ios-medkit-outline bg-black"></i><span class="sidebar-title">Error pages</span></a>
-                <ul id="collapse14" class="list-unstyled collapse">
-                    <li><a href="error-400.html">Error 400</a></li>
-                    <li><a href="error-401.html">Error 401</a></li>
-                    <li><a href="error-403.html">Error 403</a></li>
-                    <li><a href="error-404.html">Error 404</a></li>
-                    <li><a href="error-500.html">Error 500</a></li>
-                    <li><a href="error-503.html">Error 503</a></li>
-                </ul>
+
+            <li class="sidebar-category">Ayuda</li>
+            <li class="panel">
+                <a href="{{ url('/ayuda') }}">
+                    <i class="ion-ios-help-outline bg-black"></i>
+                    <span class="sidebar-title">Solicitar ayuda</span>
+                </a>
             </li>
-            <li class="panel"><a role="button" data-toggle="collapse" data-parent=".navigation" href="#collapse15" aria-expanded="false" aria-controls="collapse15" class="collapsed"><i class="ion-ios-browsers-outline bg-black"></i><span class="sidebar-title">User pages</span></a>
-                <ul id="collapse15" class="list-unstyled collapse">
-                    <li><a href="login.html">Login</a></li>
-                    <li><a href="register.html">Register</a></li>
-                    <li><a href="forgot-password.html">Forgot password</a></li>
-                    <li><a href="lock-screen.html">Lock screen</a></li>
-                    <li><a href="profile.html">Profile</a></li>
-                </ul>
-            </li>
-            <li class="sidebar-category">Help</li>
-            <li class="panel"><a href="documentation.html"><i class="ion-ios-help-outline bg-black"></i><span class="sidebar-title">Documentation</span></a></li>
         </ul>
+
         <div class="sidebar-category">Downloads</div>
         <div class="sidebar-widget">
             <ul class="list-unstyled pl-15 pr-15">
