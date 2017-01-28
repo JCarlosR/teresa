1f7410b2dfc7fca6f03bfe6218ab1428b4834794
Juan Carlos <juancagb.17@gmail.com>
2017-01-28 11:01:56 -0500

    Admin mobile option

---
+++ resources/views/includes/panel_notification_bar.blade.php
@@ -0,0 +1,119 @@
+<ul class="notification-bar list-inline pull-right">
+    <li class="visible-xs"><a href="javascript:;" role="button" class="header-icon search-bar-toggle"><i class="ion-search"></i></a></li>
+    {{--<li class="visible-lg"><a href="javascript:;" role="button" class="header-icon sidebar-control"><i class="ion-pin"></i></a></li>--}}
+    {{--<li class="dropdown"><a id="dropdownMenu0" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" class="dropdown-toggle bubble header-icon"><i class="ion-code-working"></i><span class="badge bg-danger">4</span></a>--}}
+        {{--<div aria-labelledby="dropdownMenu0" class="dropdown-menu dropdown-menu-right dm-small animated fadeInUp">--}}
+            {{--<ul data-mcs-theme="minimal-dark" class="media-list mCustomScrollbar">--}}
+                {{--<li class="media">--}}
+                    {{--<a href="javascript:;">--}}
+                        {{--<div class="block clearfix mb-10"><span class="pull-left">Datos principales</span><span class="pull-right label label-outline label-success">80% Completo</span></div>--}}
+                        {{--<div class="progress progress-xs mb-0">--}}
+                            {{--<div role="progressbar" data-transitiongoal="80" class="progress-bar progress-bar-success"></div>--}}
+                        {{--</div>--}}
+                    {{--</a>--}}
+                {{--</li>--}}
+                {{--<li class="media">--}}
+                    {{--<a href="javascript:;">--}}
+                        {{--<div class="block clearfix mb-10"><span class="pull-left">Servicios</span><span class="pull-right label label-outline label-danger">20% Completo</span></div>--}}
+                        {{--<div class="progress progress-xs mb-0">--}}
+                            {{--<div role="progressbar" data-transitiongoal="20" class="progress-bar progress-bar-danger"></div>--}}
+                        {{--</div>--}}
+                    {{--</a>--}}
+                {{--</li>--}}
+                {{--<li class="media">--}}
+                    {{--<a href="javascript:;">--}}
+                        {{--<div class="block clearfix mb-10"><span class="pull-left">Proyectos</span><span class="pull-right label label-outline label-primary">65% Completo</span></div>--}}
+                        {{--<div class="progress progress-xs mb-0">--}}
+                            {{--<div role="progressbar" data-transitiongoal="65" class="progress-bar"></div>--}}
+                        {{--</div>--}}
+                    {{--</a>--}}
+                {{--</li>--}}
+                {{--<li class="media">--}}
+                    {{--<a href="javascript:;">--}}
+                        {{--<div class="block clearfix mb-10"><span class="pull-left">Pagos realizados</span><span class="pull-right label label-outline label-purple">45% Completo</span></div>--}}
+                        {{--<div class="progress progress-xs mb-0">--}}
+                            {{--<div role="progressbar" data-transitiongoal="45" class="progress-bar progress-bar-purple"></div>--}}
+                        {{--</div>--}}
+                    {{--</a>--}}
+                {{--</li>--}}
+            {{--</ul>--}}
+        {{--</div>--}}
+    {{--</li>--}}
+    <li class="dropdown"><a id="dropdownMenu1" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" class="dropdown-toggle bubble header-icon"><i class="ion-alert"></i><span class="badge bg-danger">3</span></a>
+        <div aria-labelledby="dropdownMenu1" class="dropdown-menu dropdown-menu-right dm-medium animated fadeInUp">
+            <h5 class="dropdown-header">Tienes 6 notificaciones</h5>
+            <ul data-mcs-theme="minimal-dark" class="media-list mCustomScrollbar">
+                <li class="media"><a href="javascript:;">
+                        <div class="media-left"><i class="ion-chatbubbles media-object mo-md img-circle bg-primary text-center"></i></div>
+                        <div class="media-body">
+                            <h5 class="media-heading">Edo Esaine</h5>
+                            <p class="text-muted mb-0">Te envió un mensaje</p>
+                        </div>
+                        <div class="media-right text-nowrap">
+                            <time datetime="2016-12-10T20:27:48+07:00" class="fs-13 text-muted">5 mins</time>
+                        </div>
+                    </a>
+                </li>
+                <li class="media"><a href="javascript:;">
+                        <div class="media-left avatar"><img src="{{ asset('build/images/users/11.jpg') }}" alt="" class="media-object img-circle"><span class="status bg-danger"></span></div>
+                        <div class="media-body">
+                            <h5 class="media-heading">Judy Fowler</h5>
+                            <p class="text-muted mb-0">Te envió un nuevo email</p>
+                        </div>
+                        <div class="media-right text-nowrap">
+                            <time datetime="2016-12-10T20:42:40+07:00" class="fs-13 text-muted">12 mins</time>
+                        </div>
+                    </a>
+                </li>
+                <li class="media"><a href="javascript:;">
+                        <div class="media-left"><span class="media-object mo-md img-circle bg-success text-center fw-700">S</span></div>
+                        <div class="media-body">
+                            <h5 class="media-heading">Sean Jenkins</h5>
+                            <p class="text-muted mb-0">Sent you a new message</p>
+                        </div>
+                        <div class="media-right text-nowrap">
+                            <time datetime="2016-12-10T20:50:48+07:00" class="fs-13 text-muted">8 hours</time>
+                        </div>
+                    </a>
+                </li>
+                <li class="media"><a href="javascript:;">
+                        <div class="media-left"><i class="ion-person media-object mo-md img-circle bg-purple text-center"></i></div>
+                        <div class="media-body">
+                            <h5 class="media-heading">Jean Lawrence</h5>
+                            <p class="text-muted mb-0">Sent you a new email</p>
+                        </div>
+                        <div class="media-right text-nowrap">
+                            <time datetime="2016-12-10T20:42:40+07:00" class="fs-13 text-muted">20 Oct</time>
+                        </div>
+                    </a>
+                </li>
+                <li class="media"><a href="javascript:;">
+                        <div class="media-left avatar"><img src="{{ asset('build/images/users/13.jpg') }}" alt="" class="media-object img-circle"><span class="status bg-warning"></span></div>
+                        <div class="media-body">
+                            <h5 class="media-heading">Karen Reed</h5>
+                            <p class="text-muted mb-0">Commented on your post</p>
+                        </div>
+                        <div class="media-right text-nowrap">
+                            <time datetime="2016-12-10T20:27:48+07:00" class="fs-13 text-muted">20 Oct</time>
+                        </div>
+                    </a>
+                </li>
+            </ul>
+            <div class="dropdown-footer"><a href="javascript:;" class="text-muted">View all notifications</a></div>
+        </div>
+    </li>
+    <li class="dropdown hidden-sm hidden-xs"><a id="dropdownMenu2" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" class="dropdown-toggle header-icon lh-1 pt-10 pb-10">
+            <div class="media mt-0">
+                <div class="media-left avatar"><img src="{{ asset(auth()->user()->photo_route) }}" alt="" class="media-object img-circle"><span class="status bg-success"></span></div>
+                <div class="media-right media-middle pl-0">
+                    <p class="fs-12 mb-0">Hola, {{ auth()->user()->short_name }}</p>
+                </div>
+            </div></a>
+        <ul aria-labelledby="dropdownMenu2" class="dropdown-menu dropdown-menu-right animated fadeInUp icon">
+            <li><a href="{{ url('/datos/principales') }}"><i class="ion-person mr-5"></i> Mis datos</a></li>
+            <li><a href="{{ url('/leads') }}"><i class="ion-filing mr-5"></i> Leads</a></li>
+            <li><a href="{{ url('/logout') }}"><i class="ion-unlocked mr-5"></i> Cerrar sesión</a></li>
+        </ul>
+    </li>
+    <li><a href="javascript:;" role="button" class="right-sidebar-toggle bubble header-icon"><i class="ion-arrow-return-right"></i><span class="badge bg-danger">4</span></a></li>
+</ul>
