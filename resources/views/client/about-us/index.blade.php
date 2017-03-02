2f2a1ae83222404aee38bd630211f7494b2c3905
Juan Carlos <juancagb.17@gmail.com>
2017-03-02 10:38:41 -0500

    Status pull right

--- resources/views/client/about-us/index.blade.php
+++ resources/views/client/about-us/index.blade.php
@@ -45,28 +45,28 @@
                         ¿Qué tipo de empresa es?
                     </h3>
                     <span id="limit1"></span>
-                    <span id="status1"></span>
+                    <span id="status1" class="pull-right"></span>
                     <textarea id="note1" title="Pregunta 1" name="question_1">{{ old('question_1', $about_us->question_1) }}</textarea>
 
                     <h3>
                         ¿Cuál es su especialización?
                     </h3>
                     <span id="limit2"></span>
-                    <span id="status2"></span>
+                    <span id="status2" class="pull-right"></span>
                     <textarea id="note2" title="Pregunta 2" name="question_2">{{ old('question_2', $about_us->question_2) }}</textarea>
 
                     <h3>
                         ¿Cuándo, cómo y dónde se creó la empresa?
                     </h3>
                     <span id="limit3"></span>
-                    <span id="status3"></span>
+                    <span id="status3" class="pull-right"></span>
                     <textarea id="note3" title="Pregunta 3" name="question_3">{{ old('question_3', $about_us->question_3) }}</textarea>
 
                     <h3>
                         ¿Qué servicios ofrecen?
                     </h3>
                     <span id="limit4"></span>
-                    <span id="status4"></span>
+                    <span id="status4" class="pull-right"></span>
                     <textarea id="note4" title="Pregunta 4" name="question_4">{{ old('question_4', $about_us->question_4) }}</textarea>
 
                     <h3>
@@ -74,7 +74,7 @@
                         <small>Misión</small>
                     </h3>
                     <span id="limit5"></span>
-                    <span id="status5"></span>
+                    <span id="status5" class="pull-right"></span>
                     <textarea id="note5" title="Pregunta 5" name="question_5">{{ old('question_5', $about_us->question_5) }}</textarea>
 
                     <h3>
@@ -82,7 +82,7 @@
                         <small>Visión, a corto y largo plazo</small>
                     </h3>
                     <span id="limit6"></span>
-                    <span id="status6"></span>
+                    <span id="status6" class="pull-right"></span>
                     <textarea id="note6" title="Pregunta 6" name="question_6">{{ old('question_6', $about_us->question_6) }}</textarea>
                 </fieldset>
 
