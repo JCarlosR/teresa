866c8ef639c860b871b2bddd2badf64022a90d23
Juan Carlos <juancagb.17@gmail.com>
2017-04-06 10:28:32 -0500

    ajax error message

--- public/vendor/SocialCounters/js/api.js
+++ public/vendor/SocialCounters/js/api.js
@@ -338,8 +338,24 @@
               $('#wrapper [data-social="linkedIn"] .count').text(k);
               getTotal(followers);
           },
-          error: function (data) {
-              console.log(data);
+          error: function (jqXHR, exception) {
+              var msg = '';
+              if (jqXHR.status === 0) {
+                  msg = 'Not connect. Verify Network.';
+              } else if (jqXHR.status == 404) {
+                  msg = 'Requested page not found. [404]';
+              } else if (jqXHR.status == 500) {
+                  msg = 'Internal Server Error [500].';
+              } else if (exception === 'parsererror') {
+                  msg = 'Requested JSON parse failed.';
+              } else if (exception === 'timeout') {
+                  msg = 'Time out error.';
+              } else if (exception === 'abort') {
+                  msg = 'Ajax request aborted.';
+              } else {
+                  msg = 'Uncaught Error: ' + jqXHR.responseText;
+              }
+              console.log(msg);
           }
       });
     }
