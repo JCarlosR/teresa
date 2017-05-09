67a8a1ac35d3a970bacf962dc70e247fcadb38b7
Juan Carlos <juancagb.17@gmail.com>
2017-05-09 11:32:24 -0500

    Show only services ID

--- resources/views/themes/default/welcome.blade.php
+++ resources/views/themes/default/welcome.blade.php
@@ -83,27 +83,27 @@
                     <div id="Container">
                         @foreach ($me->projects as $project)
                             @if ($project->featuredImage)
-                            <div class="filimg mix {{ $project->services }} col-md-4 col-sm-4 col-xs-12" data-myorder="{{ $project->id }}">
+                            <div class="filimg mix @foreach ($project->services as $service) category-{{ $service->id }}  @endforeach col-md-4 col-sm-4 col-xs-12" data-myorder="{{ $project->id }}">
                                 <img src="{{ $project->featuredImage->fullPath }}" class="img-responsive" title="{{ $project->featuredImage->name }}">
                             </div>
                             @endif
                         @endforeach
-                        <div class="filimg mix category-1 category-3 col-md-4 col-sm-4 col-xs-12" data-myorder="2">
-                            <img src="{{ asset('/themes/default/img/fea1.jpg') }}" class="img-responsive">
-                        </div>
-                        <div class="filimg mix category-2 col-md-4 col-sm-4 col-xs-12" data-myorder="4">
-                            <img src="{{ asset('/themes/default/img/fea2.jpg') }}" class="img-responsive">
-                        </div>
-                        <div class="filimg mix category-1 col-md-4 col-sm-4 col-xs-12" data-myorder="1">
-                            <img src="{{ asset('/themes/default/img/fea3.jpg') }}" class="img-responsive">								</div>
-                        <div class="filimg mix category-2 category-3 col-md-4 col-sm-4 col-xs-12" data-myorder="8">
-                            <img src="{{ asset('/themes/default/img/fea4.jpg') }}" class="img-responsive">								</div>
-                        <div class="filimg mix category-2 col-md-4 col-sm-4 col-xs-12" data-myorder="8">
-                            <img src="{{ asset('/themes/default/img/fea5.jpg') }}" class="img-responsive">
-                        </div>
-                        <div class="filimg mix category-2 category-3 col-md-4 col-sm-4 col-xs-12" data-myorder="8">
-                            <img src="{{ asset('/themes/default/img/fea6.jpg') }}" class="img-responsive">
-                        </div>
+                        {{--<div class="filimg mix category-1 category-3 col-md-4 col-sm-4 col-xs-12" data-myorder="2">--}}
+                            {{--<img src="{{ asset('/themes/default/img/fea1.jpg') }}" class="img-responsive">--}}
+                        {{--</div>--}}
+                        {{--<div class="filimg mix category-2 col-md-4 col-sm-4 col-xs-12" data-myorder="4">--}}
+                            {{--<img src="{{ asset('/themes/default/img/fea2.jpg') }}" class="img-responsive">--}}
+                        {{--</div>--}}
+                        {{--<div class="filimg mix category-1 col-md-4 col-sm-4 col-xs-12" data-myorder="1">--}}
+                            {{--<img src="{{ asset('/themes/default/img/fea3.jpg') }}" class="img-responsive">								</div>--}}
+                        {{--<div class="filimg mix category-2 category-3 col-md-4 col-sm-4 col-xs-12" data-myorder="8">--}}
+                            {{--<img src="{{ asset('/themes/default/img/fea4.jpg') }}" class="img-responsive">								</div>--}}
+                        {{--<div class="filimg mix category-2 col-md-4 col-sm-4 col-xs-12" data-myorder="8">--}}
+                            {{--<img src="{{ asset('/themes/default/img/fea5.jpg') }}" class="img-responsive">--}}
+                        {{--</div>--}}
+                        {{--<div class="filimg mix category-2 category-3 col-md-4 col-sm-4 col-xs-12" data-myorder="8">--}}
+                            {{--<img src="{{ asset('/themes/default/img/fea6.jpg') }}" class="img-responsive">--}}
+                        {{--</div>--}}
                     </div>
                 </div>
             </div>
