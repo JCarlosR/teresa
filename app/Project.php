903ec14f80692a5182eef5d61c77c19f4889349e
Juan Carlos <juancagb.17@gmail.com>
2017-05-24 09:45:10 -0500

    The featured image is checked in the theme

--- app/Project.php
+++ app/Project.php
@@ -41,12 +41,12 @@ class Project extends Model
     {
         $projectImages = $this->images;
 
-        if (count($projectImages) == 0) {
-            $projectImage = new ProjectImage();
-            $projectImage->fullPath = url('images/teresa-placeholder.png');
-            $projectImage->name = 'Proyeto sin imagen asignada';
-            return $projectImage;
-        }
+//        if (count($projectImages) == 0) {
+//            $projectImage = new ProjectImage();
+//            $projectImage->fullPath = url('images/teresa-placeholder.png');
+//            $projectImage->name = 'Proyeto sin imagen asignada';
+//            return $projectImage;
+//        }
 
         foreach ($projectImages as $projectImage) {
             if ($projectImage->featured)
