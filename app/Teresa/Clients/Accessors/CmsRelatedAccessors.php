a40c61c83ad8d2c4456da1c17b97b5e41b04935c
Juan Carlos <juancagb.17@gmail.com>
2017-06-06 16:17:51 -0500

    Show services filter in projects

--- app/Teresa/Clients/Accessors/CmsRelatedAccessors.php
+++ app/Teresa/Clients/Accessors/CmsRelatedAccessors.php
@@ -7,10 +7,14 @@ trait CmsRelatedAccessors
 
     public function getLinkTo($path)
     {
-        if (strpos(url('/'), 'teresa') !== false) { // it is not the same that === true :D
+        // !==false is not the same that ===true
+
+        // if contains 'teresa' or contains 'localhost'
+        if (strpos(url('/'), 'teresa') !== false || strpos(url('/'), 'localhost') !== false) {
             return url('/ver/' . $this->id . $path);
         }
 
+        // for specific client domains
         return url($path);
     }
 
--- resources/views/themes/default/projects/index.blade.php
+++ resources/views/themes/default/projects/index.blade.php
@@ -12,25 +12,39 @@
     </div>
     <!--CTA1 END-->
 
-    <!--PORTFOLIO START-->
     <div id="portfolio" class="section-padding">
         <div class="container">
             <div class="row">
-                <div class="page-title">
-                    <ol>
-                        @foreach ($projects as $project)
-                            <li>
-                                <a href="{{ $me->getLinkTo('/proyecto/'.$project->id) }}">
-                                    {{ $project->name }}
-                                </a>.
-                                <p>{{ $project->description }}</p>
-                                <hr class="pg-titl-bdr-btm">
-                            </li>
+                <div class="page-title text-center">
+                    <h2>Nuestros proyectos</h2>
+                    <p>{{ $me->projects_description }}</p>
+                    <p><a href="{{ $me->getLinkTo('/proyectos') }}" class="btn btn-default">Ver más</a></p>
+                    <hr class="pg-titl-bdr-btm">
+                </div>
+                <div class="port-sec">
+                    <div class="col-md-12 fil-btn text-center">
+                        <div class="filter wrk-title active" data-filter="all">Ver todos</div>
+                        @foreach ($me->services as $service)
+                            <div class="filter wrk-title" data-filter=".category-{{ $service->id }}">
+                                {{ $service->name }}
+                            </div>
                         @endforeach
-                    </ol>
+                    </div>
+                    <div id="Container">
+                        @foreach ($me->projects as $project)
+                            @if ($project->featuredImage)
+                                <div class="filimg mix @foreach ($project->services as $service) category-{{ $service->id }}  @endforeach col-md-4 col-sm-4 col-xs-12" data-myorder="{{ $project->id }}">
+                                    <h4 class="text-center">{{ $project->name }}</h4>
+                                    <a href="{{ $me->getLinkTo('/proyecto/'.$project->id) }}">
+                                        <img src="{{ $project->featuredImage->fullPath }}" class="img-responsive" title="{{ $project->featuredImage->name }}">
+                                    </a>
+                                    <p class="text-center">{{ $project->description }}</p>
+                                </div>
+                            @endif
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <!--PORTFOLIO END-->
 @endsection
