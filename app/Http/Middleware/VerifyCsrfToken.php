23f8edf199be32c86627ad09ec79032d0582a1e7
Juan Carlos <juancagb.17@gmail.com>
2017-06-07 11:41:56 -0500

    Handle VerifyCsrfToken middleware exception

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -4,6 +4,8 @@ namespace App\Http\Middleware;
 
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
 
+use Closure;
+
 class VerifyCsrfToken extends BaseVerifier
 {
     /**
@@ -14,4 +16,20 @@ class VerifyCsrfToken extends BaseVerifier
     protected $except = [
         //
     ];
+
+    // override the default handle method
+    public function handle($request, Closure $next)
+    {
+        if (
+            $this->isReading($request) ||
+            $this->runningUnitTests() ||
+            $this->shouldPassThrough($request) ||
+            $this->tokensMatch($request)
+        ) {
+            return $this->addCookieToResponse($request, $next($request));
+        }
+
+        // throw new TokenMismatchException;
+        return back()->withError('Oops, el formulario no se ha enviado a tiempo. Por favor inténtalo de nuevo.');
+    }
 }
