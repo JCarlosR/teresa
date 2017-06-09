993ed82999cb986ff9bc512aef58675a96d19d0a
Juan Carlos <juancagb.17@gmail.com>
2017-06-09 10:49:05 -0500

    Remove useless data-table dependencies

--- resources/views/client/dashboard.blade.php
+++ resources/views/client/dashboard.blade.php
@@ -65,14 +65,6 @@
     {{-- Data tables --}}
     <script type="text/javascript" src="{{ asset('plugins/datatables.net/js/jquery.dataTables.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('plugins/datatables.net-bs/js/dataTables.bootstrap.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('plugins/datatables.net-buttons/js/dataTables.buttons.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('plugins/datatables.net-buttons-bs/js/buttons.bootstrap.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('plugins/jszip/dist/jszip.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('plugins/pdfmake/build/pdfmake.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('plugins/pdfmake/build/vfs_fonts.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('plugins/datatables.net-buttons/js/buttons.colVis.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('plugins/datatables.net-buttons/js/buttons.print.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('plugins/datatables.net-buttons/js/buttons.html5.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('plugins/datatables.net-responsive/js/dataTables.responsive.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('plugins/datatables.net-responsive-bs/js/responsive.bootstrap.js') }}"></script>
     {{-- Dashboard logic --}}
