131fdfa76019b90d9260f0f8f23d54e2b1f0ef35
Juan Carlos <juancagb.17@gmail.com>
2017-06-27 11:15:32 -0500

    Update name to env variable

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Bienvenido a Teresa v1.0</div>
+                <div class="panel-heading">Bienvenido a {{ env('APP_NAME', 'Theressa') }}</div>
 
                 <div class="panel-body text-center">
                     <img src="{{ asset('/images/teresa-placeholder.png') }}" class="img-responsive" style="margin: 0 auto" alt="Teresa - Texto" width="380">
