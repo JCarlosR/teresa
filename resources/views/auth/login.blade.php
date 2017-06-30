181c1f0ce8687b9a518422e4c875eb5e025a74a3
Juan Carlos <juancagb.17@gmail.com>
2017-06-30 09:30:12 -0500

    New logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,8 +3,7 @@
 @section('content')
 <div class="page-content">
     <div class="logo">
-        <h1>TERESA v1.0</h1>
-        {{--<img src="{{ asset('build/images/logo/logo-dark.png') }}" alt="Logo" width="160">--}}
+        <img src="{{ asset('/images/logo.jpg') }}" alt="Logo" width="180">
     </div>
     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
         {{ csrf_field() }}
@@ -40,21 +39,8 @@
         </div>
         <button type="submit" class="btn btn-success btn-rounded btn-block">Iniciar sesión</button>
     </form>
-    {{--<hr>--}}
-    {{--<p class="text-muted">Sign in with your Facebook or Twitter accounts</p>--}}
-    {{--<div class="clearfix">--}}
-        {{--<div class="pull-left">--}}
-            {{--<button type="button" style="width: 130px" class="btn btn-outline btn-rounded btn-primary"><i class="ion-social-facebook mr-5"></i> Facebook</button>--}}
-        {{--</div>--}}
-        {{--<div class="pull-right">--}}
-            {{--<button type="button" style="width: 130px" class="btn btn-outline btn-rounded btn-info"><i class="ion-social-twitter mr-5"></i> Twitter</button>--}}
-        {{--</div>--}}
-    {{--</div>--}}
-    <hr>
-    <div class="clearfix">
-        <p class="text-muted mb-0 pull-left">¿Deseas registrarte?</p>
-        <a href="{{ url('/register') }}" class="inline-block pull-right">Registrarme</a>
-    </div>
+
+
 </div>
 @endsection
 
