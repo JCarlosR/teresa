be57ccfe7021e779d10131884f23e71a95f17e69
Juan Carlos <juancagb.17@gmail.com>
2017-07-14 12:16:06 -0500

    Pass param to included view

--- resources/views/includes/user/services/tree.blade.php
+++ resources/views/includes/user/services/tree.blade.php
@@ -3,7 +3,7 @@
         <a href="#">{{ $service->name }}</a>
         @if ($service->services)
         <ul>
-            @include('includes.user.services.tree')
+            @include('includes.user.services.tree', ['services' => $service->services])
         </ul>
         @endif
     </li>
