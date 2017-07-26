a5bff0b8607f16e82c1235d20deb2f5e7058344b
Juan Carlos <juancagb.17@gmail.com>
2017-07-26 11:36:28 -0500

    Display phone in message

--- resources/views/client/inbox/show.blade.php
+++ resources/views/client/inbox/show.blade.php
@@ -5,10 +5,10 @@
         <div class="row">
             <div class="col-sm-6">
                 <div class="media">
-                    <div class="media-left avatar"><img src="/build/images/users/12.jpg" alt="{{ $message->name }}" class="media-object img-circle"><span class="status bg-success"></span></div>
+                    <div class="media-left avatar"><img src="/images/users/default.jpg" alt="{{ $message->name }}" class="media-object img-circle"><span class="status bg-success"></span></div>
                     <div style="width:auto" class="media-body">
                         <h5 class="media-heading">{{ $message->name }}</h5>
-                        <p class="text-muted mb-0"><a href="#">{{ $message->email }}</a> a mí</p>
+                        <p class="text-muted mb-0">de <a href="#">{{ $message->email }}</a> para mí</p>
                     </div>
                 </div>
             </div>
@@ -18,15 +18,12 @@
                         <button type="button" class="btn btn-outline btn-default">Responder</button>
                         <button type="button" data-toggle="dropdown" aria-expanded="false" class="btn btn-outline btn-default dropdown-toggle"><span class="caret"></span><span class="sr-only">Toggle Dropdown</span></button>
                         <ul role="menu" class="dropdown-menu pull-right animated fadeInDown">
-                            <li><a href="#">Reply</a></li>
-                            <li><a href="#">Reply To All</a></li>
-                            <li><a href="#">Forward</a></li>
-                            <li><a href="#">Something else here</a></li>
+                            <li><a href="#">Responder</a></li>
                             <li class="divider"></li>
-                            <li><a href="#">Print</a></li>
-                            <li><a href="#">Mark as spam</a></li>
-                            <li><a href="#">Mark as unread</a></li>
-                            <li><a href="#">Delete this message</a></li>
+                            <li><a href="#">Imprimir</a></li>
+                            <li><a href="#">Marcar como spam</a></li>
+                            <li><a href="#">Marcar como no leído</a></li>
+                            <li><a href="#">Eliminar este mensaje</a></li>
                         </ul>
                     </div>
                     <div role="group" aria-label="Second group" class="btn-group">
@@ -40,6 +37,8 @@
     <div class="email-single">
         <h3 class="fw-400">{{ $message->topic }}</h3>
         <p>{{ $message->content }}</p>
+        <hr>
+        <p><strong>Teléfono de contacto:</strong> {{ $message->phone }}</p>
         <hr>
         <div class="text-right">
             <a href="{{ url('/inbox') }}" type="button" class="btn btn-default">
