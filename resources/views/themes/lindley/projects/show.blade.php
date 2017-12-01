36292328e93986d3e9b81a0935808233a39dc1ee
roel cc <roel@seo-arquitectos.com>
2017-11-30 20:42:07 -0500

    questionshow style

--- resources/views/themes/lindley/projects/show.blade.php
+++ resources/views/themes/lindley/projects/show.blade.php
@@ -45,8 +45,6 @@
                             <h3> CARACTERÍSTICAS TÉCNICAS</h3>
 
 
-                            {{--<small>¿Qué detalles técnicos especificarías?</small>--}}
-                           <p> {!! $project->question_3 !!}</p>
                         </div>
                     </div>
                     <div class="col-md-4 pad40">
@@ -101,8 +99,10 @@
                             </li>
                         </ul>
                         <br>
-                        {{--<h4>CARACTERÍSTICAS TÉCNICAS</h4>--}}
+                        <h4>CARACTERÍSTICAS TÉCNICAS</h4>
 
+                        {{--<small>¿Qué detalles técnicos especificarías?</small>--}}
+                        <p> {!! $project->question_3 !!}</p>
 
 
                     </div>
