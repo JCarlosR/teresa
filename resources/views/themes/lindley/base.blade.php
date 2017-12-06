f37680e8e6eb826440955af39eb0c8352642e1ca
roel cc <roel@seo-arquitectos.com>
2017-12-06 15:35:34 -0500

    transform uppercase service

--- resources/views/themes/lindley/base.blade.php
+++ resources/views/themes/lindley/base.blade.php
@@ -31,7 +31,7 @@
     <link rel="stylesheet" href=" https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css">
-    <link rel="stylesheet" type="text/css" href="{{ asset('/themes/lindley/css/style.css?v=32') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/themes/lindley/css/style.css?v=34') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('/themes/lindley/css/icons.css') }}">
     @if ($me->favicon)
         <link rel="shortcut icon" type="image/x-icon" href="{{ $me->favicon_url }}">
