20c9ce6216e3c570febbe3b67279028c4180acba
roel cc <roel@seo-arquitectos.com>
2017-12-06 12:30:46 -0500

    cambios de service nav short name

--- resources/views/themes/lindley/includes/service-nav.blade.php
+++ resources/views/themes/lindley/includes/service-nav.blade.php
@@ -3,17 +3,17 @@
     <div>
 
         <ul class="nav-list">
-            {{--@foreach ($me->services as $item)--}}
-            {{--<li><a href="{{ $me->getLinkTo('/servicio/'.$item->id) }}">{{ $item->name }}<i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>--}}
-            {{--@endforeach--}}
+            @foreach ($me->services as $item)
+            <li><a href="{{ $me->getLinkTo('/servicio/'.$item->id) }}">{{ $item->short_name }}<i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>
+            @endforeach
 
-                <li><a href="">TIENDA RETAIL <i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>
-                <li><a href="">DESARROLLO INTEGRAL <i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>
-                <li><a href="">IMPLEMENTACION TIENDA <i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>
-                <li><a href="">MANTENIEMIENTO TIENDA <i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>
-                <li><a href="">PLANOS SEGURIDAD <i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>
-                <li><a href="">SUPERVISION PROYECTOS <i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>
-                <li><a href="">SISTEMA SEGURIDAD <i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>
+                {{--<li><a href="">TIENDA RETAIL <i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>--}}
+                {{--<li><a href="">DESARROLLO INTEGRAL <i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>--}}
+                {{--<li><a href="">IMPLEMENTACION TIENDA <i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>--}}
+                {{--<li><a href="">MANTENIEMIENTO TIENDA <i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>--}}
+                {{--<li><a href="">PLANOS SEGURIDAD <i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>--}}
+                {{--<li><a href="">SUPERVISION PROYECTOS <i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>--}}
+                {{--<li><a href="">SISTEMA SEGURIDAD <i class="fa fa-chevron-right" aria-hidden="true"></i></a></li>--}}
 
         </ul>
     </div>
